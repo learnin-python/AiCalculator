ae43b0dac6e1ea76afb9a21e3657ec7d3390977b
Le Bon Bauma <baumabulondo@gmail.com>
2021-01-12 15:09:46 +0300

    Update details on rename for existing default

--- main.py
+++ main.py
@@ -25,16 +25,20 @@ Bot = ChatBot(name='Calculator',
               read_only=True,
               logic_adapters=["chatterbot.logic.MathematicalEvaluation"],
               storage_adapter='chatterbot.storage.SQLStorageAdapter')
-
+# clear the screen and start the calculator
 print('\033c')
 print("Hello, I am a calculator. How may I help you?")
 while True:
+    # take the input from the user
     user_input = input("me: ")
 
+    # check if the user has typed quit to exit the program
     if user_input.lower() == 'quit':
         print("Existing")
         break
 
+    # otherwise, evaluate the user input
+    # print invalid input if the AI is unable to comprehend the input
     try:
         response = Bot.get_response(user_input)
         print("Calculator", response)
